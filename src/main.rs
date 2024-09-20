649dde806138ea8736db5fdaa97b8cb33e7f2183
Willy Micieli <otechdo@otechdo.com>
2024-09-20 04:09:49 +0200

    clean code

--- src/main.rs
+++ src/main.rs
@@ -1,171 +1,164 @@
-#![allow(clippy::multiple_crate_versions)]
-
-use chrono::Utc;
-use inquire::{Confirm, Text};
-use std::env::args;
-use std::fs::{remove_file, File, OpenOptions};
-use std::io;
+use clap::{Arg, ArgMatches, Command};
+use std::fs::{read_to_string, remove_file, File, OpenOptions};
 use std::io::Write;
-use std::io::{stdin, BufRead};
-use std::path::Path;
-use std::process::{exit, Command, ExitCode};
 
-const TYPE: &str = "&str";
-const NAME: &str = "vec.rs";
-
-///
-/// # Panics
-///
-fn read_lines(filename: &str) -> io::Lines<io::BufReader<File>> {
-    let file: File = File::open(filename).expect("failed to open filename");
-    io::BufReader::new(file).lines()
+pub fn new() -> ArgMatches {
+    Command::new("vec-new")
+        .bin_name("vec-new")
+        .about("create a rust vec using command line")
+        .arg(
+            Arg::new("run")
+                .long("run")
+                .short('r')
+                .help("the command to create vec")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::new("comment")
+                .long("comment")
+                .short('c')
+                .help("the const comment")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::new("name")
+                .long("name")
+                .short('n')
+                .help("the name of the constant to create")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::new("type")
+                .long("type")
+                .short('t')
+                .help("the type of the constant to create")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::new("output")
+                .long("output")
+                .short('o')
+                .help("the output to the vec file")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::new("append")
+                .long("append")
+                .short('a')
+                .help("append the new vec the vec file")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::new("edit")
+                .long("edit")
+                .short('e')
+                .help("edit file the vec file after the creation")
+                .required(true)
+                .require_equals(true),
+        )
+        .get_matches()
 }
-
-fn parse_file_lines(filename: &str) -> Vec<String> {
-    let mut file_lines: Vec<String> = Vec::new();
-    read_lines(filename).for_each(|line| match line {
-        Ok(l) => {
-            file_lines.push(l);
-        }
-        Err(x) => println!("{x}"),
-    });
-    file_lines
-}
-
-///
-/// # Panics
-///
-#[must_use]
-pub fn exec(cmd: &str, args: &[&str]) -> bool {
-    Command::new(cmd)
-        .args(args)
-        .spawn()
-        .unwrap()
-        .wait()
-        .expect("failed to execute cmd")
-        .success()
-}
-
-fn get_file() -> String {
-    format!(
-        "/tmp/{}.rs",
-        Utc::now().to_string().split_whitespace().next().expect("")
-    )
-}
-
-fn run(dest: &str, src: &str, const_name: &str) -> ExitCode {
-    gen(dest, src, parse_file_lines(src).len(), const_name)
-}
-
-fn const_name() -> String {
-    let args: Vec<String> = args().collect();
-    args.get(2)
-        .expect("Constant name is missing")
-        .to_uppercase()
-}
-
-fn write(file: &str) -> File {
-    let f = OpenOptions::new().append(true).open(file).unwrap();
-    f
-}
-
-fn gen(dest: &str, src: &str, lines: usize, constant: &str) -> ExitCode {
-    if Path::new(dest).exists() {
-        remove_file(dest).expect("failed to remove");
-    }
-    assert!(File::create(dest).is_ok());
-
-    let mut f = write(dest);
-
-    writeln!(f, "pub const {constant} : [{TYPE};{lines}]  = [").expect("Failed to create vector");
-    for line in &parse_file_lines(src) {
-        writeln!(f, "\"{line}\",").expect("Failed to create vector");
-    }
-    writeln!(f, "];").expect("Failed to create vector");
-    assert!(exec("sh", &["-c", format!("xdg-open {dest}").as_str()]));
-    remove_file(src).expect("failed to remove");
-    exit(0);
-}
-
-fn out() -> ExitCode {
-    let dest = get_file();
-    let mut lines: Vec<String> = Vec::new();
-    stdin().lines().for_each(|x| lines.push(x.unwrap()));
-    if Path::new("vec").exists() {
-        remove_file("vec").expect("failed to remove");
-    }
-    assert!(File::create("vec").is_ok());
-    let mut f = write(dest.as_str());
-    writeln!(f, "pub const NEW : [{TYPE};{}]  = [", lines.len()).expect("Failed to create vector");
-    for line in &lines {
-        writeln!(f, "\"{line}\",").expect("Failed to create vector");
-    }
-    writeln!(f, "];").expect("Failed to create vector");
-    assert!(exec("sh", &["-c", format!("xdg-open {dest}").as_str()]));
-    assert!(remove_file("vec").is_ok());
-    exit(0);
-}
-
-fn main() -> ExitCode {
-    let args: Vec<String> = args().collect();
-    if args.len() == 1 {
-        return out();
-    }
-    if args.len() == 2 && args.get(1).unwrap().eq("--interactive") {
-        let mut constant: String = String::new();
-        loop {
-            constant.clear();
-            constant.push_str(
-                Text::new("Please enter the constant name : ")
-                    .prompt()
-                    .unwrap()
-                    .to_uppercase()
-                    .as_str(),
-            );
-            if !constant.is_empty() {
-                break;
+fn main() {
+    let x = new();
+    if let Some(name) = x.get_one::<String>("name") {
+        if let Some(comment) = x.get_one::<String>("comment") {
+            if let Some(typ) = x.get_one::<String>("type") {
+                if let Ok(save) = File::create("const.rs") {
+                    if let Some(command) = x.get_one::<String>("run") {
+                        if let Some(output) = x.get_one::<String>("output") {
+                            if let Some(append) = x.get_one::<String>("append") {
+                                let cmd =
+                                    command.split_whitespace().collect::<Vec<&str>>().join(" ");
+                                if let Ok(mut cmd) = std::process::Command::new("sh")
+                                    .stdout(save)
+                                    .arg("-c")
+                                    .arg(cmd)
+                                    .current_dir(".")
+                                    .spawn()
+                                {
+                                    assert!(cmd.wait().is_ok());
+                                    if append.eq("true") {
+                                        if let Ok(content) = read_to_string("const.rs") {
+                                            let lines = content.lines().collect::<Vec<&str>>();
+                                            let size = lines.len();
+                                            if let Ok(mut file) = OpenOptions::new()
+                                                .append(true)
+                                                .write(true)
+                                                .create(true)
+                                                .open(output)
+                                            {
+                                                assert!(writeln!(
+                                                    file,
+                                                    "\n#[doc = \"{comment}\"]\nconst {} : {typ} = [{typ};{size}] = [",
+                                                    name.to_uppercase()
+                                                )
+                                                .is_ok());
+                                                for line in &lines {
+                                                    if typ.eq("&str") {
+                                                        assert!(
+                                                            writeln!(file, "\"{line}\",").is_ok()
+                                                        );
+                                                    } else {
+                                                        assert!(writeln!(file, "{line},").is_ok());
+                                                    }
+                                                }
+                                                assert!(writeln!(file, "];").is_ok());
+                                                assert!(remove_file("const.rs").is_ok());
+                                            }
+                                        }
+                                    } else {
+                                        if let Ok(mut file) = OpenOptions::new()
+                                            .append(false)
+                                            .write(true)
+                                            .create(true)
+                                            .open(output)
+                                        {
+                                            if let Ok(content) = read_to_string("const.rs") {
+                                                let lines = content.lines().collect::<Vec<&str>>();
+                                                let size = lines.len();
+                                                assert!(writeln!(
+                                                    file,
+                                                    "\n#[doc = \"{comment}\"]\nconst {} : {typ} = [{typ};{size}] = [",
+                                                    name.to_uppercase()
+                                                )
+                                                .is_ok());
+                                                for line in &lines {
+                                                    if typ.eq("&str") {
+                                                        assert!(
+                                                            writeln!(file, "\"{line}\",").is_ok()
+                                                        );
+                                                    } else {
+                                                        assert!(writeln!(file, "{line},").is_ok());
+                                                    }
+                                                }
+                                                assert!(writeln!(file, "];").is_ok());
+                                                assert!(remove_file("const.rs").is_ok());
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if let Some(edit) = x.get_one::<String>("edit") {
+                                if edit.eq("true") {
+                                    if let Ok(mut editor) = std::process::Command::new("vim")
+                                        .arg(output.as_str())
+                                        .current_dir(".")
+                                        .spawn()
+                                    {
+                                        assert!(editor.wait().is_ok());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        if Path::new(NAME).exists() {
-            remove_file(NAME).unwrap();
-        }
-        assert!(File::create_new(NAME).is_ok());
-        let mut f = OpenOptions::new()
-            .append(true)
-            .open(NAME)
-            .expect("failed to open file");
-        loop {
-            let data = Text::new("Enter the value append in vec : ")
-                .prompt()
-                .expect("");
-            assert!(f.write(format!("{data}\n").as_bytes()).is_ok());
-            if Confirm::new("Add an other data to vec ? ")
-                .with_default(true)
-                .prompt()
-                .unwrap()
-                .eq(&false)
-            {
-                break;
-            }
-        }
-        return run(get_file().as_str(), NAME, constant.as_str());
     }
-    if args.len() == 3 {
-        return run(
-            get_file().as_str(),
-            args.get(1).unwrap().as_str(),
-            const_name().as_str(),
-        );
-    }
-    if args.len() == 2 {
-        return run(
-            get_file().as_str(),
-            args.get(1).unwrap().as_str(),
-            args.get(1).unwrap().to_uppercase().as_str(),
-        );
-    }
-    println!("command | vec-new");
-    println!("vec-new <file>");
-    println!("vec-new <file> <constant_name>");
-    exit(1);
 }
